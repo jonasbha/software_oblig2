8c506e478197bc75b11362c1ce954e2f9f85f870
Jonas BH <jonas.b.halvorsen@hiof.no>
2020-10-01 13:53:17 +0200

    added testcases to 400

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,5 +1,24 @@
+/*
+Et år er et skuddår:
+
+- Når det er delelig med 4, men ikke 100
+- Når det er delelig med 400
+(For eksempel var år 2000 et skuddår)
+
+Et år er ikke et skuddår:
+
+- Når det ikke er delelig med 4
+- Når det er delelig med 100, men ikke 400.
+(For eksempel var ikke 1700, 1800 og 1900 skuddår, ei heller blir 2100 et)
+* */
+
 public class LeapYear {
-    public static boolean isLeapYear(int input) {
-        return true;
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && !(year % 100 == 0)) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/testLeapYear.java
+++ src/test/java/testLeapYear.java
@@ -5,11 +5,28 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 import static org.junit.jupiter.params.provider.Arguments.*;
 
+/*
+Et år er et skuddår:
+
+- Når det er delelig med 4, men ikke 100
+- Når det er delelig med 400
+(For eksempel var år 2000 et skuddår)
+
+Et år er ikke et skuddår:
+
+- Når det ikke er delelig med 4
+- Når det er delelig med 100, men ikke 400.
+(For eksempel var ikke 1700, 1800 og 1900 skuddår, ei heller blir 2100 et)
+* */
+
 public class testLeapYear {
     private static Stream<Arguments> useCases() {
         return Stream.of(
                 of(true, 4),
-                of(false, 100)
+                of(true, 40),
+                of(true, 400),
+                of(false, 100),
+                of(false, 300)
         );
     }
 
